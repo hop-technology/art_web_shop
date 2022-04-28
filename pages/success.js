2c3fd56368edd6be0ed252d40920ca92590a251d
lukeperrin10 <lukeperrin27@gmail.com>
2022-04-28 15:03:00 +0200

    updates code to include customer information for deliveries and orders

--- pages/success.js
+++ pages/success.js
@@ -25,45 +25,62 @@ function SuccessPage() {
   return success ? (
     <div className='success'>
       <h1 className='success__message'>Thank you for your order!</h1>
-      <table className='success__container'>
-        <thead className='success__header'>
-          <tr>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Total Price</th>
-          </tr>
-        </thead>
-        {order.order.orderItems.map((item, index) => {
-          return (
-            <tbody key={index}>
-              <tr className='success__content'>
-                <td>
-                  <Image
-                    src={item.product.images[0].url}
-                    height='100'
-                    width='100'
-                  />
-                </td>
-                <td>
-                  <p>{item.product.name}</p>
-                </td>
-                <td>
-                  <p>{item.quantity}</p>
-                </td>
-                <td>{item.total / 100}</td>
+      <div className='success__product-information'>
+        <div>
+          <h2>Delivery details</h2>
+          <p>
+            {order.order.name}
+            <br></br>
+            {order.order.address}
+            <br></br>
+            {order.order.email}
+            <br></br>
+          </p>
+        </div>
+        <div>
+          <table className='success__container'>
+            <thead className='success__header'>
+              <tr>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Total Price</th>
               </tr>
-            </tbody>
-          )
-        })}
-      </table>
-      <div className='success__information'>
-        <h2 className='success__total'>Total Sum: {order.order.total}</h2>
-        <h3>
-          Please contact us at{' '}
-          <a href='artshop@walborgventures.com'>artshop@walborgventures.com</a>{' '}
-          or 08-123 56 78 for any questions.
-        </h3>
+            </thead>
+            {order.order.orderItems?.map((item, index) => {
+              return (
+                <tbody key={index}>
+                  <tr className='success__content'>
+                    <td>
+                      <Image
+                        src={item.product.images[0].url}
+                        height='100'
+                        width='100'
+                      />
+                    </td>
+                    <td>
+                      <p>{item.product.name}</p>
+                    </td>
+                    <td>
+                      <p>{item.quantity}</p>
+                    </td>
+                    <td>{item.total / 100}</td>
+                  </tr>
+                </tbody>
+              )
+            })}
+          </table>
+        </div>
+        <div className='success__information'>
+          <h2 className='success__total'>Total Sum: {order.order.total}</h2>
+          <h3>
+            Please contact us at{' '}
+            <a href='artshop@walborgventures.com'>
+              artshop@walborgventures.com
+            </a>{' '}
+            or 08-123 56 78 for any questions.
+          </h3>
+        </div>
       </div>
     </div>
   ) : (
