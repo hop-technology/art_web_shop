77f1275ad4f3e48e6753e8b8b6c11728aa3d5a99
lukeperrin10 <lukeperrin27@gmail.com>
2022-05-18 09:18:38 +0200

    adds a contact page for company information adds styling to cart nav

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Image from 'next/image'
 import Link from 'next/link'
 import MobNav from './MobNav'
 import HopHelper from '../../pages/api/helpers'
@@ -15,6 +16,7 @@ const Navbar = () => {
   const location = router.asPath
   const homeActive = ['/'].includes(location) ? 'active' : ''
   const cartActive = ['/cart'].includes(location) ? 'active' : ''
+  const contactActive = ['/contact'].includes(location) ? 'active' : ''
 
   const logit = () => {
     setScrollY(window.pageYOffset)
@@ -48,17 +50,24 @@ const Navbar = () => {
           </Link>
         </div>
         <div className='navbar__links--link'>
-          <Link href='/cart'>
-            <a className={`navbar__links--a ${cartActive}`}>
-              <p>
-                Cart
-                <span className='navbar__cart'>
-                  {HopHelper.totalAmount(items)}
-                </span>
-              </p>
+          <Link href='/contact'>
+            <a className={`navbar__links--a ${contactActive}`}>
+              <p>Kontakt</p>
             </a>
           </Link>
         </div>
+      </div>
+      <div className='navbar__links--link'>
+        <Link href='/cart'>
+          <a className={`navbar__links--a ${cartActive}`}>
+            <Image src='/shopping-cart.svg' height={30} width={30} />
+            <div className='navbar__cart'>
+              <span className='amount'>
+                <p>{HopHelper.totalAmount(items)}</p>
+              </span>
+            </div>
+          </a>
+        </Link>
       </div>
     </div>
   )
---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className='contact__container'>
+        <h1>Get in touch</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
